396f9f137692f4342b1360af4d1ddb9e76fa3909
WesCourtney44 <wescourtney44@gmail.com>
2016-10-10 21:24:57 -0400

    Web Files are now an Internal Project Folder
    Also added a shortcut to the Web Files folder.

--- Visual Studio/1945 TypeScript Clone/Web Files/Main.js
+++ Visual Studio/1945 TypeScript Clone/Web Files/Main.js
@@ -6,7 +6,7 @@ class Main {
         let c = document.getElementById("myCanvas");
         let ctx = c.getContext("2d");
         ctx.beginPath();
-        ctx.arc(95, 50, 30, 0, 2 * Math.PI);
+        ctx.arc(95, 50, 60, 0, 2 * Math.PI);
         ctx.stroke();
     }
 }
--- Visual Studio/1945 TypeScript Clone/Web Files/Main.ts
+++ Visual Studio/1945 TypeScript Clone/Web Files/Main.ts
@@ -7,7 +7,7 @@ class Main {
         let c: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myCanvas");
         let ctx: CanvasRenderingContext2D = c.getContext("2d");
         ctx.beginPath();
-        ctx.arc(95, 50, 30, 0, 2 * Math.PI);
+        ctx.arc(95, 50, 60, 0, 2 * Math.PI);
         ctx.stroke();
     }
 }
